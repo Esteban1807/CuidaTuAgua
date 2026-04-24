49a849ab132c616417941c3d8409a04d3ebbb0a8
EstebanPC <juanome43@gmail.com>
2026-04-24 11:28:52 -0500

    First progress with navbar from landingpage

--- FrontEnd/CuidaTuAgua/screens/LandingScreen.styles.ts
+++ FrontEnd/CuidaTuAgua/screens/LandingScreen.styles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { colors} from '../theme/Colors';
+import { colors, spacing, typography} from '../theme/index';
 
 export const styles = StyleSheet.create({
     safeArea: {
@@ -7,19 +7,32 @@ export const styles = StyleSheet.create({
         backgroundColor: colors.surface,
     },
     nav: {
-        height: 70,
-       
+        height: 80,
+        
+      
     },
     logo:{
         flex: 1,
+        maxWidth: 500,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     logoImage: {
-        width: 120,
-        height: 80,
-        resizeMode: 'contain',
-        justifyContent: 'center',
+        width: 80,
+        height: 70,
+        resizeMode: 'contain',        
+    },
+
+    logoTitle:{
+        ...typography.title,
+        color: colors.textSecondary,
         
     },
+    title:{
+
+    },
+    
     acceder: {
         flex: 1,
         justifyContent: 'center',
